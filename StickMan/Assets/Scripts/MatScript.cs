7d555d1c11e7a6993282a341b05e4001ee3a5f70
2380carlos <carlos.2380@hotmail.com>
2020-09-19 21:45:46 +0200

    Added Mat animation

---
+++ StickMan/Assets/Scripts/MatScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatScript : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        animator.SetTrigger("Touched");
+    }
+}
