501c6dbdeb319172c169c09a580d9e66c96221ac
2380carlos <carlos.2380@hotmail.com>
2020-09-20 18:23:04 +0200

    Added soft movment camera. Change background and player sprite.

--- StickMan/Assets/Scripts/CtrlCamera.cs
+++ StickMan/Assets/Scripts/CtrlCamera.cs
@@ -5,9 +5,38 @@ using UnityEngine;
 public class CtrlCamera : MonoBehaviour
 {
     public GameObject player;
+    public float speed;
+    public float MaxOffset;
 
+    private float offset;
+
+    private Rigidbody2D playerRigidbody;
+
+    private void Start()
+    {
+        offset = 0;
+        playerRigidbody = player.GetComponent<Rigidbody2D>();
+        gameObject.transform.position = new Vector3(player.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z); ;
+    }
     void Update()
     {
-        gameObject.transform.position = new Vector3(player.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);  ;
+         
+        if(playerRigidbody.velocity.x > 0)
+        {
+            offset += speed*Time.deltaTime;
+            if(offset > MaxOffset)
+            {
+                offset = MaxOffset;
+            }
+        }
+        else if (playerRigidbody.velocity.x < 0)
+        {
+            offset -= speed * Time.deltaTime;
+            if (offset < -MaxOffset)
+            {
+                offset = -MaxOffset;
+            }
+        }
+        gameObject.transform.position = new Vector3(player.transform.position.x + offset, gameObject.transform.position.y, gameObject.transform.position.z);
     }
 }
