ea8ac880460066f9a3d532d6b0a1f7951a67abee
2380carlos <carlos.2380@hotmail.com>
2020-09-20 16:37:34 +0200

    Basic ctrlCamera and modify 2 sprites

---
+++ StickMan/Assets/Scripts/CtrlCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CtrlCamera : MonoBehaviour
+{
+    public GameObject player;
+
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(player.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);  ;
+    }
+}
